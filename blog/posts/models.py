d19753d7625ea184de41e60c0e4002cb4f39291e
MasumTech <masumrezadiu@gmail.com>
2020-02-07 03:55:19 +0600

    Update blog/posts/models.py in Django

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,15 +1,44 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
+from django.db.models.signals import pre_save
+from django.conf import settings
 
 # Create your models here.
-from django.urls import reverse
+from django.utils import timezone
+
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
+
+
+
+def upload_location(instance, filename):
+    return "%s/%s" %(instance.id, filename)
 
 
 class Post(models.Model):
-    objects = None
+
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to="upload_location",
+                              blank=True,
+                              width_field = "width_field",
+                              null=True,
+                              height_field="height_field")
+    width_field = models.IntegerField(default=0)
+    height_field = models.IntegerField(default=0)
     content = models.TextField()
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
     updated = models.DateTimeField(auto_now=True, auto_now_add=False)
     timestamp = models.DateTimeField(auto_now=False, auto_now_add = True)
+
+
+    objects = PostManager()
 
     def __unicode__(self):
         return self.title
@@ -19,3 +48,24 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("detail", kwargs={"qid": self.id})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" % (slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+   if not instance.slug:
+       instance.slug = create_slug(instance)
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
