e8dd4981f59c0e5f2bcd78281e27d13d84b7e982
MasumTech <masumrezadiu@gmail.com>
2020-02-07 03:48:22 +0600

    Update blog/posts/views.py in Django

--- blog/posts/views.py
+++ blog/posts/views.py
@@ -1,15 +1,26 @@
-from django.http import HttpResponseRedirect
+from django.db.models import Q
+from django.utils import timezone
+from six.moves.urllib.parse import quote
+from django.http import HttpResponseRedirect, Http404, HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from .forms import PostForm
 from .models import Post
+from django.core.paginator import Paginator
+from django.shortcuts import render
+
+
 # Create your views here.
 
 
 def post_create(request):
-    form = PostForm(request.POST or None)
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
+
+    form = PostForm(request.POST or None, request.FILES or None)
     if form.is_valid():
         instance = form.save(commit=False)
+        instance.user = request.user
         instance.save()
         messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
@@ -22,25 +33,51 @@ def post_create(request):
 
 def post_detail(request, qid):
     instance = get_object_or_404(Post, id=qid)
+    if instance.publish > timezone.now().date() or instance.draft:
+        if not request.user.is_staff or not request.user.is_superuser:
+            raise Http404
+
+    share_string = quote(instance.content)
     context = {
         "title": instance.title,
         "instance": instance,
+        "share_string": share_string,
     }
     return render(request, "post_detail.html", context)
 
 
 def post_list(request):
-    queryset = Post.objects.all()
+    today = timezone.now().date()
+    queryset_list = Post.objects.active() #.order_by('-timestamp')
+    if request.user.is_staff or request.user.is_superuser:
+        queryset_list = Post.objects.all()
+
+    query = request.GET.get("q")
+    if query:
+        queryset_list = queryset_list.filter(
+            Q(title__icontains=query) |
+            Q(content__icontains=query) |
+            Q(user__first_name__icontains=query) |
+            Q(user__last_name__icontains=query)
+             ).distinct()
+
+    paginator = Paginator(queryset_list, 2)  # Show 25 contacts per page.
+    page_number = request.GET.get('page')
+    queryset = paginator.get_page(page_number)
     context = {
         "object_list": queryset,
-        "title": "List"
+        "title": "List",
+        "today": today,
     }
     return render(request, 'post_list.html', context)
 
 
+
 def post_update(request, qid=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=qid)
-    form = PostForm(request.POST or None, instance=instance)
+    form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
@@ -56,6 +93,8 @@ def post_update(request, qid=None):
 
 
 def post_delete(request, qid=None):
+    if not request.user.is_staff or not request.user.is_superuser:
+        raise Http404
     instance = get_object_or_404(Post, id=qid)
     instance.delete()
     messages.success(request, "Successfully Deleted")
